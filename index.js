b9e11a09228bbc033d51e42f2392b04902413ca6
Travis Brown <bergannation@gmail.com>
2021-05-22 16:26:44 -0500

    updating with operating code

--- index.js
+++ index.js
@@ -249,7 +249,7 @@ function init() {
             <meta charset='UTF-8'>
             <meta http-equiv='X-UA-Compatible' content='IE=edge'>
             <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-            <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css' rel='stylesheet' crossorigin='anonymous'>
+            <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css' rel='stylesheet'>
             <link rel="stylesheet" href="./Assets/style.css"/>
             <title>The 'A' Team</title>
         </head>
